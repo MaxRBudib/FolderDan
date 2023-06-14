3f737818ebeca849dc32b6fee1ed14f584b517c3
MaxRBudib <83791565+MaxRBudib@users.noreply.github.com>
2023-06-14 10:03:35 -0600

    fixed routing f

--- Docker/backend/programaster/programaster/settings.py
+++ Docker/backend/programaster/programaster/settings.py
@@ -148,7 +148,7 @@ DATABASES = {
         'NAME': 'Programaster',
         'USER': 'postgres',
         'PASSWORD': 'Prueba#123',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
